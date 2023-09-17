260a18fdd2b93b693d1472c78627411d309c4feb
Siddhartha <siddharthakomakula@gmail.com>
2023-09-17 13:14:05 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fileSystem = require('fs')
+const importJSON = fileSystem.readFileSync('budget.json', 'utf8');
+const budgetData = JSON.parse(importJSON);
 
-app.use('/', express.static('public'));
-
-const budget = {
-   myBudget: [
-  {
-    title: 'Rent',
-    budget: 125
-  },
-  {
-    title: 'medicines',
-    budget: 375
-  },
-
-  {
-    title:'Grocery',
-    budget: 140
-  },
-]
-};
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', (req, res) => 
+{
+res.send('Hello World!');
 });
 
-app.get('/budget', (req,res) => {
-  res.json(budget);
+app.listen(port, () =>
+{
+console.log(`Example app listening at http://localhost:${port}`);
+}
+);
+
+app.get('/budget', (req, res) => {
+    res.json(budgetData);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+app.use('/',express.static('public'));
